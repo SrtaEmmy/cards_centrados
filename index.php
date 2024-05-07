72e67ce5881c2158b3c809842decf465ad3e0229
srtaemmy <programming.emmy123@gmail.com>
2024-05-07 14:25:45 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<style>
+    body{
+        background-color: #ccc;
+    }
+</style>
+</head>
+
+<body>
+
+    <div class="container">
+        <h1>Cards centrados</h1>
+
+        <div class="container-cards d-flex row">
+
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+            <div class="card m-2" style="width: 18rem;">
+                <img src="tiendaVideo.webp" class="card-img-bottom card-img-top mt-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">card's content.</p>
+                    <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+                </div>
+            </div>
+ 
+        </div>
+    </div>
+
+</body>
+
+</html>
